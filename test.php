a4325e1a68b99baa28cae6bb91d7af43d05c7c7c
sean <slang800@gmail.com>
2012-04-06 14:22:09 -0500

    added test file

--- test.php
+++ test.php
@@ -1,6 +1,29 @@
 <?php
+require 'path.php';
 
+$path = [
+	'html',//TODO: add manifest="manifest.mf" + make file
+	[
+		'head',
+		[
+			'meta',
+			'http-equiv' => "Content-Type",
+			'content' => "text/html",
+			'charset' => "utf-8"
+		],
+		['title', 'CSD'],
+		[
+			'link#favicon',
+			'href' => 'favicon.ico',
+			'rel' => 'shortcut icon',
+		],
+		'<!--[if lt IE 9]><link rel="stylesheet" type="text/css" href="css/style-ie.css" /><![endif]-->',
+		//TODO: add meta tags for bookmarks and/or for search engines
+	],
+	[
+		'body#body'
+	]
+];
 
-
-
+echo path($path);
 ?>
